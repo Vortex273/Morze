06921fb7ff2c7bbbf833f05aee83d9b6e8cc830d
rdkasimov <ol.kasimov33@gmail.com>
2025-01-27 21:50:48 +0400

    all

--- main.py
+++ main.py
@@ -25,6 +25,32 @@ MorseCode = {"a": ".-",
              "y": "-.--",
              "z": "--.."}
 
+MorseCodeInversion = {".-": "a",
+             "-...": "b",
+             "-.-.": "c",
+             "-..": "d",
+             ".": "e",
+             "..-.": "f",
+             "--.": "g",
+             "....": "h",
+             "..": "i",
+             ".---": "j",
+             "-.-": "k",
+             ".-..": "l",
+             "--": "m",
+             "-.": "n",
+             "---": "o",
+             ".--.": "p",
+             "--.-": "q",
+             ".-.": "r",
+             "...": "s",
+             "-": "t",
+             "..-": "u",
+             "...-": "v",
+             ".--": "w",
+             "-..-": "x",
+             "-.--": "y",
+             "--..": "z"}
 
 def encode_to_morse(text):
     text = text.lower()
@@ -32,4 +58,8 @@ def encode_to_morse(text):
         print(*[MorseCode[j] for j in i])
 
 def decode_from_morse(code):
-    pass
+    code = code.lower().split()
+    for i in code:
+        print(*[MorseCode[j] for j in i])
+
+
