70edacdf769b0a7a2b34aaa58359a806759136dd
Vortex273 <hooker.deef@gmail.com>
2025-01-30 14:33:06 +0400

    Update main.py
    Я чуть подправил и main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+text = ""
+code = ""
 MorseCode = {"a": ".-",
              "b": "-...",
              "c": "-.-.",
@@ -26,40 +28,60 @@ MorseCode = {"a": ".-",
              "z": "--.."}
 
 MorseCodeInversion = {".-": "a",
-             "-...": "b",
-             "-.-.": "c",
-             "-..": "d",
-             ".": "e",
-             "..-.": "f",
-             "--.": "g",
-             "....": "h",
-             "..": "i",
-             ".---": "j",
-             "-.-": "k",
-             ".-..": "l",
-             "--": "m",
-             "-.": "n",
-             "---": "o",
-             ".--.": "p",
-             "--.-": "q",
-             ".-.": "r",
-             "...": "s",
-             "-": "t",
-             "..-": "u",
-             "...-": "v",
-             ".--": "w",
-             "-..-": "x",
-             "-.--": "y",
-             "--..": "z"}
+                      "-...": "b",
+                      "-.-.": "c",
+                      "-..": "d",
+                      ".": "e",
+                      "..-.": "f",
+                      "--.": "g",
+                      "....": "h",
+                      "..": "i",
+                      ".---": "j",
+                      "-.-": "k",
+                      ".-..": "l",
+                      "--": "m",
+                      "-.": "n",
+                      "---": "o",
+                      ".--.": "p",
+                      "--.-": "q",
+                      ".-.": "r",
+                      "...": "s",
+                      "-": "t",
+                      "..-": "u",
+                      "...-": "v",
+                      ".--": "w",
+                      "-..-": "x",
+                      "-.--": "y",
+                      "--..": "z"}
+
 
 def encode_to_morse(text):
     text = text.lower()
     for i in text.split():
         print(*[MorseCode[j] for j in i])
 
+
 def decode_from_morse(code):
     code = code.lower().split()
     for i in code:
-        print(*[MorseCode[j] for j in i])
+        print(*[MorseCodeInversion[j] for j in i])
 
 
+def main():
+    global text
+    global code
+    print("Вы хотите закодировать или раскодировать текст?"
+          "\nЕсли закодировать введите:  1"
+          "\nЕсли раскодировать введите: 2")
+    a = input()
+    if a == "1":
+        print("Теперь введите текст")
+        text = input()
+        encode_to_morse(text)
+    elif a == "2":
+        print("Теперь введите код")
+        code = input()
+        decode_from_morse(code)
+    else:
+        print("Введите 1 или 2")
+        return
